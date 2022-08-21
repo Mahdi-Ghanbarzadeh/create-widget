c41625e0fafec147e439273ca1141546ccbc17f5
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2022-08-21 17:01:42 +0430

    change Break by field

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import {
   Select,
   Input,
   Button,
-  Dropdown,
-  Menu,
   Card,
   Row,
   Col,
-  Space,
   InputNumber,
   Divider,
 } from "antd";
@@ -101,6 +98,8 @@ function CreateWidget() {
 
   const [source, setSource] = useState(fieldData[sourceData[0]]);
   const [field, setField] = useState(fieldData[sourceData[0]][0]);
+  const [breakDisable, setBreakDisable] = useState(false);
+  const [breakData, setBreakData] = useState(["Time"]);
 
   console.log("source: " + source);
   console.log("field: " + field);
@@ -109,6 +108,7 @@ function CreateWidget() {
     console.log("handleSourceChange Run!");
     setSource(fieldData[value]);
     setField(fieldData[value][0]);
+    // console.log(onFinish());
   };
 
   const onFieldChange = (value) => {
@@ -119,11 +119,63 @@ function CreateWidget() {
     console.log("Received values of form: ", values);
   };
 
-  const getField = (city) => {
-    console.log("getField Run!");
-    console.log(city);
-    // console.log(form);
+  const handleBreakDisable = () => {
+    // console.log();
+    // console.log("group by: ", form.getFieldValue("group_by"));
+    // console.log("group by: ", form.getFieldValue("group_by").length);
+    // if (form.getFieldValue("group_by") === undefined) return false;
+    // else return form.getFieldValue("group_by").length > 2;
+    setBreakDisable(form.getFieldValue("group_by").length > 2);
+  };
+
+  const handleBreakData = () => {
+    console.log("---");
+    console.log(form.getFieldValue("group_by"));
+    console.log(breakData);
+    const data = form.getFieldValue("group_by");
+    const len = data.length;
+    console.log(data);
+    console.log(len);
+    if (len === 1) {
+      // console.log("len 1");
+      // console.log(data);
+      // console.log(data[0]["select-group"]);
+      // const ddtest = data[0]["select-group"];
+      // if (ddtest === "Time") {
+      //   console.log("return");
+      //   return;
+      // }
+      // const dd = data.map((d) => d["select-group"]);
+      // // const ddtime = dd.filter((f) => f !== "Time");
+      // console.log("--dd--");
+      // console.log(dd);
+      // dd.push("Time");
+      // console.log(dd);
+      // setBreakData(data);
+      console.log("len 1");
+      const dd = data.map((d) => d["select-group"]);
+      console.log(dd);
+      if (!dd.includes("Time")) dd.push("Time");
+      setBreakData(dd);
+      data.map((d) => console.log(d.value));
+    } else if (len === 2) {
+      console.log("len 2");
+      const dd = data.map((d) => d["select-group"]);
+      console.log(dd);
+      setBreakData(dd);
+      data.map((d) => console.log(d.value));
+    }
+    // console.log("group by: ", form.getFieldValue("group_by"));
+    // console.log("group by: ", form.getFieldValue("group_by").length);
+    // if (form.getFieldValue("group_by") === undefined) return false;
+    // else return form.getFieldValue("group_by").length > 2;
+    // setBreakData(form.getFieldValue("group_by").length > 2);
+  };
+
+  const getField = () => {
     form.setFieldsValue({ operand_field: null });
+    form.setFieldsValue({ group_by: null });
+    form.setFieldsValue({ order_by: null });
   };
 
   return (
@@ -135,9 +187,7 @@ function CreateWidget() {
           name="validate_other"
           onFinish={onFinish}
           initialValues={{
-            "input-number": 3,
-            "checkbox-group": ["A", "B"],
-            rate: 3.5,
+            source: sourceData[0],
           }}
         >
           <Form.Item
@@ -146,7 +196,7 @@ function CreateWidget() {
             rules={[{ required: true, message: "Please select your source!" }]}
           >
             <Select
-              defaultValue={sourceData[0]}
+              // defaultValue={sourceData[0]}
               style={{
                 width: 150,
               }}
@@ -156,39 +206,40 @@ function CreateWidget() {
               {sourceData.map((source) => (
                 <Option key={source}>{source}</Option>
               ))}
-              {/* <Option value="source1">source1</Option>
-              <Option value="source2">source2</Option>
-              <Option value="source3">source3</Option> */}
             </Select>
           </Form.Item>
-
           <Row>
-            <Col span={12}>
+            <Col span={13}>
               <Card
                 title="Operand"
                 style={{
-                  width: 600,
+                  width: 650,
                 }}
               >
-                <Form.Item
-                  name="operand_function"
-                  label="Function"
-                  rules={[
-                    { required: true, message: "Please select your function!" },
-                  ]}
-                >
-                  <Select
-                    style={{
-                      width: 150,
-                    }}
-                  >
-                    <Option value="count">count</Option>
-                    <Option value="values">values</Option>
-                  </Select>
-                </Form.Item>
+                <Row gutter={16}>
+                  <Col>
+                    <Form.Item
+                      name="operand_function"
+                      label="Function"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please select your function!",
+                        },
+                      ]}
+                    >
+                      <Select
+                        style={{
+                          width: 100,
+                        }}
+                      >
+                        <Option value="count">count</Option>
+                        <Option value="values">values</Option>
+                      </Select>
+                    </Form.Item>
+                  </Col>
 
-                <Row>
-                  <Col span={12}>
+                  <Col>
                     <Form.Item
                       name="operand_field"
                       label="Field"
@@ -199,11 +250,9 @@ function CreateWidget() {
                         },
                       ]}
                     >
-                      {/* {console.log("field+: " + field)} */}
-                      {/* {field} */}
                       <Select
                         style={{
-                          width: 150,
+                          width: 120,
                         }}
                         value={field}
                         onChange={onFieldChange}
@@ -211,28 +260,30 @@ function CreateWidget() {
                         {source.map((src) => (
                           <Option key={src}>{src}</Option>
                         ))}
-                        {/* {console.log("field+: " + field)} */}
-                        {/* <Option value="field1">field1</Option>
-                        <Option value="field2">field2</Option> */}
                       </Select>
                     </Form.Item>
                   </Col>
-                  <Col span={12}>
+
+                  <Col>
                     <Form.Item name="operand_label" label="Label">
-                      <Input />
+                      <Input
+                        style={{
+                          width: 150,
+                        }}
+                      />
                     </Form.Item>
                   </Col>
                 </Row>
               </Card>
             </Col>
-            <Col span={12}>
+            <Col span={11}>
               <Card
                 title="Group By"
                 style={{
                   width: 600,
                 }}
               >
-                <Row>
+                {/* <Row>
                   <Col span={11}>
                     <Form.Item name="group_field" label="Field">
                       <Select
@@ -250,12 +301,20 @@ function CreateWidget() {
                       <Input />
                     </Form.Item>
                   </Col>
-                </Row>
-                <Form.List name="users">
+                </Row> */}
+                <Form.List name="group_by">
                   {(fields, { add, remove }, { errors }) => (
                     <>
+                      {/* {console.log("-----")}
+                      {console.log(fields)}
+                      {console.log("add " + add)} */}
+                      {/* {fields.length === 0 ? add() : ""}
+                      {fields.length === 0
+                        ? console.log("true")
+                        : console.log("false")} */}
                       {fields.map((field, index) => (
                         <Row>
+                          {console.log(field.name)}
                           <Col span={11}>
                             <Form.Item
                               name={[field.name, "select-group"]}
@@ -265,9 +324,11 @@ function CreateWidget() {
                                 style={{
                                   width: 150,
                                 }}
+                                onChange={handleBreakData}
                               >
-                                <Option value="field1">field1</Option>
-                                <Option value="field2">field2</Option>
+                                {source.map((src) => (
+                                  <Option key={src}>{src}</Option>
+                                ))}
                               </Select>
                             </Form.Item>
                           </Col>
@@ -282,7 +343,10 @@ function CreateWidget() {
                           <Col span={2}>
                             <MinusCircleOutlined
                               className="dynamic-delete-button"
-                              onClick={() => remove(field.name)}
+                              onClick={() => {
+                                remove(field.name);
+                                handleBreakDisable();
+                              }}
                             />
                           </Col>
                         </Row>
@@ -290,7 +354,10 @@ function CreateWidget() {
                       <Form.Item>
                         <Button
                           type="dashed"
-                          onClick={() => add()}
+                          onClick={() => {
+                            add();
+                            handleBreakDisable();
+                          }}
                           style={{
                             width: "100%",
                           }}
@@ -307,51 +374,47 @@ function CreateWidget() {
               </Card>
             </Col>
           </Row>
-
           <br />
           <br />
-
+          {console.log("---D---")}
+          {/* {console.log(onDisable())} */}
           <Form.Item name="break-by" label="Break by">
             <Select
               style={{
                 width: 150,
               }}
+              disabled={breakDisable}
             >
-              <Option value="source1">source1</Option>
+              {breakData.map((source) => (
+                <Option key={source}>{source}</Option>
+              ))}
+              {/* <Option value="source1">source1</Option>
               <Option value="source2">source2</Option>
-              <Option value="source3">source3</Option>
+              <Option value="source3">source3</Option> */}
             </Select>
           </Form.Item>
-
-          <Form.Item name="order-by" label="Order by">
+          <Form.Item name="order_by" label="Order by">
             <Select
               style={{
                 width: 150,
               }}
             >
-              <Option value="source1">source1</Option>
+              {source.map((src) => (
+                <Option key={src}>{src}</Option>
+              ))}
+              {/* <Option value="source1">source1</Option>
               <Option value="source2">source2</Option>
-              <Option value="source3">source3</Option>
+              <Option value="source3">source3</Option> */}
             </Select>
           </Form.Item>
-
           <Form.Item
             name="limit"
-            // {...formItemLayout}
             label="Limit"
-            // validateStatus={number.validateStatus}
-            // help={number.errorMsg}
             rules={[{ type: "number", min: 1 }]}
           >
-            <InputNumber
-            // min={1}
-            // value={number.value} dont use value !!!! describe mohandes
-            // onChange={onNumberChange}
-            />
+            <InputNumber />
           </Form.Item>
-
           <Divider />
-
           <Form.Item
             name="time"
             label="Time"
@@ -362,19 +425,10 @@ function CreateWidget() {
           >
             <Input />
           </Form.Item>
-
-          <Form.Item
-            name="filters"
-            label="Filters"
-            // style={{
-            //   width: 250,
-            // }}
-          >
+          <Form.Item name="filters" label="Filters">
             <Input />
           </Form.Item>
-
           <Divider />
-
           <Row justify="center">
             <Col span={2}>
               <Form.Item>
@@ -424,7 +478,6 @@ function CreateWidget() {
               </Form.Item>
             </Col>
           </Row>
-
           <Form.Item>
             <Button type="primary" htmlType="submit">
               Submit
@@ -447,17 +500,7 @@ function CreateWidget() {
           <Col span={2} push={1}>
             <Divider type="vertical" style={{ height: "100%" }} />
           </Col>
-          <Col span={11}>
-            {/* <div>
-              <HighchartsReact
-                highcharts={Highcharts}
-                options={options}
-                containerProps={{
-                  style: { height: "300px", width: "600px" },
-                }}
-              />
-            </div> */}
-          </Col>
+          <Col span={11}></Col>
         </Row>
       </div>
     </>
