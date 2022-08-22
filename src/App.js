baa702bbb839cecf14d1cc7f6406ed6ad2b62b6c
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2022-08-22 13:21:25 +0430

    delete debugging code

--- src/App.js
+++ src/App.js
@@ -105,48 +105,24 @@ const formItemLayoutWithOutLabel = {
 function CreateWidget() {
   const [form] = Form.useForm();
 
-  console.log("---sourceData + fieldData---");
-  console.log(sourceData);
-  console.log(fieldData);
-  console.log(fieldData[sourceData[0]].fields);
-  console.log(fieldData[sourceData[0]][0]);
-
   const [source, setSource] = useState(fieldData[sourceData[0]].fields);
   const [field, setField] = useState(fieldData[sourceData[0]][0]);
   const [breakDisable, setBreakDisable] = useState(false);
-  const [breakData, setBreakData] = useState(["Time"]);
+  const [breakData, setBreakData] = useState([
+    fieldData[sourceData[0]].time_column,
+  ]);
   const [numberOfGroupByFields, setNumberOfGroupByFields] = useState(0);
   const [selectedItems, setSelectedItems] = useState([]);
 
   const filteredOptions = source.filter((o) => !selectedItems.includes(o));
 
-  const setSelectedItemsTest = () => {
-    console.log("---setSelectedItemsTest---");
-    // setSelectedItems((prev) => [...prev, value]);
-    // console.log(value);
-    // setSelectedItems();
-    // if (obj !== undefined) return obj["select-group"];
-    const dataDefined = form
+  const HandleSelectedItems = () => {
+    const data = form
       .getFieldValue("group_by")
-      .filter((obj) => obj !== undefined);
-    const data = dataDefined.map((obj) => obj["select-group"]);
-    console.log(data);
+      .filter((obj) => obj !== undefined)
+      .map((obj) => obj["select-group"]);
     setSelectedItems(data);
-    // console.log(selectedItems);
   };
-
-  // const numberOfGroupByFields = useRef(0);
-
-  // const setNumberOfGroupByFields = (value) => {
-  //   numberOfGroupByFields.current = value;
-  //   console.log("---set number of group by fields---");
-  //   console.log(numberOfGroupByFields);
-  //   console.log(numberOfGroupByFields.current);
-  // };
-
-  console.log("source + field");
-  console.log(source);
-  console.log(field);
 
   const handleSourceChange = (value) => {
     console.log("handleSourceChange Run!");
@@ -157,7 +133,6 @@ function CreateWidget() {
     setField(fieldData[value].fields[0]);
     setNumberOfGroupByFields(0);
     setBreakDisable(false);
-    // console.log(onFinish());
   };
 
   const onFieldChange = (value) => {
@@ -169,51 +144,48 @@ function CreateWidget() {
   };
 
   const handleBreakDisable = () => {
-    // console.log();
-    // console.log("group by: ", form.getFieldValue("group_by"));
-    // console.log("group by: ", form.getFieldValue("group_by").length);
-    // if (form.getFieldValue("group_by") === undefined) return false;
-    // else return form.getFieldValue("group_by").length > 2;
     setBreakDisable(form.getFieldValue("group_by").length > 2);
   };
 
-  const handleBreakData = () => {
-    console.log("---");
-    console.log(form.getFieldValue("group_by"));
-    console.log(breakData);
-    const data = form.getFieldValue("group_by");
-    const len = data.length;
-    console.log(data);
-    console.log(len);
-    if (len === 1) {
-      // console.log("len 1");
-      // console.log(data);
-      // console.log(data[0]["select-group"]);
-      // const ddtest = data[0]["select-group"];
-      // if (ddtest === "Time") {
-      //   console.log("return");
-      //   return;
-      // }
-      // const dd = data.map((d) => d["select-group"]);
-      // // const ddtime = dd.filter((f) => f !== "Time");
-      // console.log("--dd--");
-      // console.log(dd);
-      // dd.push("Time");
-      // console.log(dd);
-      // setBreakData(data);
-      console.log("len 1");
-      const dd = data.map((d) => d["select-group"]);
-      console.log(dd);
-      if (!dd.includes("Time")) dd.push("Time");
-      setBreakData(dd);
-      data.map((d) => console.log(d.value));
-    } else if (len === 2) {
-      console.log("len 2");
-      const dd = data.map((d) => d["select-group"]);
-      console.log(dd);
-      setBreakData(dd);
-      data.map((d) => console.log(d.value));
-    }
+  const handleBreakData = (value) => {
+    console.log("---handle break data---");
+    console.log(value);
+    // console.log("---");
+    // console.log(form.getFieldValue("group_by"));
+    // console.log(breakData);
+    // const data = form.getFieldValue("group_by");
+    // const len = data.length;
+    // console.log(data);
+    // console.log(len);
+    // if (len === 1) {
+    //   // console.log("len 1");
+    //   // console.log(data);
+    //   // console.log(data[0]["select-group"]);
+    //   // const ddtest = data[0]["select-group"];
+    //   // if (ddtest === "Time") {
+    //   //   console.log("return");
+    //   //   return;
+    //   // }
+    //   // const dd = data.map((d) => d["select-group"]);
+    //   // // const ddtime = dd.filter((f) => f !== "Time");
+    //   // console.log("--dd--");
+    //   // console.log(dd);
+    //   // dd.push("Time");
+    //   // console.log(dd);
+    //   // setBreakData(data);
+    //   console.log("len 1");
+    //   const dd = data.map((d) => d["select-group"]);
+    //   console.log(dd);
+    //   if (!dd.includes("Time")) dd.push("Time");
+    //   setBreakData(dd);
+    //   data.map((d) => console.log(d.value));
+    // } else if (len === 2) {
+    //   console.log("len 2");
+    //   const dd = data.map((d) => d["select-group"]);
+    //   console.log(dd);
+    //   setBreakData(dd);
+    //   data.map((d) => console.log(d.value));
+    // }
     // console.log("group by: ", form.getFieldValue("group_by"));
     // console.log("group by: ", form.getFieldValue("group_by").length);
     // if (form.getFieldValue("group_by") === undefined) return false;
@@ -245,7 +217,6 @@ function CreateWidget() {
             rules={[{ required: true, message: "Please select your source!" }]}
           >
             <Select
-              // defaultValue={sourceData[0]}
               style={{
                 width: 150,
               }}
@@ -253,7 +224,9 @@ function CreateWidget() {
               onSelect={getField}
             >
               {sourceData.map((source) => (
-                <Option key={source}>{source}</Option>
+                <Option key={source} value={source}>
+                  {source}
+                </Option>
               ))}
             </Select>
           </Form.Item>
@@ -307,7 +280,9 @@ function CreateWidget() {
                         onChange={onFieldChange}
                       >
                         {source.map((src) => (
-                          <Option key={src}>{src}</Option>
+                          <Option key={src} value={src}>
+                            {src}
+                          </Option>
                         ))}
                       </Select>
                     </Form.Item>
@@ -332,41 +307,11 @@ function CreateWidget() {
                   width: 600,
                 }}
               >
-                {/* <Row>
-                  <Col span={11}>
-                    <Form.Item name="group_field" label="Field">
-                      <Select
-                        style={{
-                          width: 150,
-                        }}
-                      >
-                        <Option value="field1">field1</Option>
-                        <Option value="field2">field2</Option>
-                      </Select>
-                    </Form.Item>
-                  </Col>
-                  <Col span={11}>
-                    <Form.Item name="group_label" label="Label">
-                      <Input />
-                    </Form.Item>
-                  </Col>
-                </Row> */}
                 <Form.List name="group_by">
                   {(fields, { add, remove }, { errors }) => (
                     <>
-                      {/* {console.log("-----")}
-                      {console.log(fields)}
-                      {console.log("add " + add)} */}
-                      {/* {fields.length === 0 ? add() : ""}
-                      {fields.length === 0
-                        ? console.log("true")
-                        : console.log("false")} */}
                       {fields.map((field, index) => (
-                        <Row>
-                          {/* {setNumberOfGroupByFields(index + 1)} */}
-                          {console.log("---add limit of add---")}
-                          {console.log(field)}
-                          {console.log(index)}
+                        <Row key={index}>
                           <Col span={11}>
                             <Form.Item
                               name={[field.name, "select-group"]}
@@ -377,20 +322,15 @@ function CreateWidget() {
                                   width: 150,
                                 }}
                                 onChange={(value) => {
-                                  // handleBreakData();
-                                  // setSelectedItems((prev) => [...prev, value]);
-                                  setSelectedItemsTest();
+                                  handleBreakData(value);
+                                  HandleSelectedItems();
                                 }}
-                                // onChange={setSelectedItemsTest}
                               >
                                 {filteredOptions.map((item) => (
-                                  <Select.Option key={item} value={item}>
+                                  <Option key={item} value={item}>
                                     {item}
-                                  </Select.Option>
+                                  </Option>
                                 ))}
-                                {/* {source.map((src) => (
-                                  <Option key={src}>{src}</Option>
-                                ))} */}
                               </Select>
                             </Form.Item>
                           </Col>
@@ -406,32 +346,23 @@ function CreateWidget() {
                             <MinusCircleOutlined
                               className="dynamic-delete-button"
                               onClick={(value) => {
-                                // setSelectedItems((prev) => {
-                                //   console.log(prev);
-                                //   console.log(value);
-                                //   return prev.filter((p) => p !== value);
-                                // });
                                 remove(field.name);
                                 handleBreakDisable();
                                 setNumberOfGroupByFields((prev) => prev - 1);
-                                setSelectedItemsTest();
+                                HandleSelectedItems();
                               }}
                             />
                           </Col>
                         </Row>
                       ))}
                       <Form.Item>
-                        {console.log("---test btn---")}
-                        {console.log(numberOfGroupByFields)}
-                        {console.log(source.length)}
-                        {console.log(numberOfGroupByFields > source.length)}
                         <Button
                           type="dashed"
                           onClick={() => {
                             add();
                             handleBreakDisable();
                             setNumberOfGroupByFields((prev) => prev + 1);
-                            setSelectedItemsTest();
+                            HandleSelectedItems();
                           }}
                           style={{
                             width: "100%",
@@ -451,8 +382,7 @@ function CreateWidget() {
           </Row>
           <br />
           <br />
-          {console.log("---D---")}
-          {/* {console.log(onDisable())} */}
+
           <Form.Item name="break-by" label="Break by">
             <Select
               style={{
@@ -461,11 +391,10 @@ function CreateWidget() {
               disabled={breakDisable}
             >
               {breakData.map((source) => (
-                <Option key={source}>{source}</Option>
+                <Option key={source} value={source}>
+                  {source}
+                </Option>
               ))}
-              {/* <Option value="source1">source1</Option>
-              <Option value="source2">source2</Option>
-              <Option value="source3">source3</Option> */}
             </Select>
           </Form.Item>
           <Form.Item name="order_by" label="Order by">
@@ -475,11 +404,10 @@ function CreateWidget() {
               }}
             >
               {source.map((src) => (
-                <Option key={src}>{src}</Option>
+                <Option key={src} value={src}>
+                  {src}
+                </Option>
               ))}
-              {/* <Option value="source1">source1</Option>
-              <Option value="source2">source2</Option>
-              <Option value="source3">source3</Option> */}
             </Select>
           </Form.Item>
           <Form.Item
